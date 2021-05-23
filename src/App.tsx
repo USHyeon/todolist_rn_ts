d5f7d2a956d0f5ee6debd87c29021e00d3ef36cc
umssung <em6721@naver.com>
2021-05-24 07:04:52 +0900

    * styled-components 적용 및 navigation library install

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,53 @@ import {
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
+import {ThemeProvider} from 'styled-components';
+import styled from 'styled-components/native';
+import Theme from './Theme';
+import {NavigationContainer} from '@react-navigation/native';
 
-const App = () => {
+interface StyledProps {
+  theme: ITheme;
+}
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props: StyledProps) =>
+    props.theme && props.theme.color.black};
+`;
+
+const MainText = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  margin: 10px;
+  color: red;
+`;
+
+interface Props {}
+const App = ({}: Props) => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const {userInfo} = React.useContext<IUserContext>(UserContext)
+
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView>
-        <View>
-          <Text>typescript hello</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <ThemeProvider theme={Theme}>
+          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+          <ScrollView>
+            <Container>
+              <MainText onPress={() => console.log('click')}>
+                Hello world TypeScript
+              </MainText>
+            </Container>
+          </ScrollView>
+        </ThemeProvider>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
